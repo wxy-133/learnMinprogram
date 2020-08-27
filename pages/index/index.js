65d302254aef244fbf763b039f242ac429348473
wxy-133 <w15279621208@163.com>
2020-08-27 19:13:51 +0800

    tarbar配置

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,84 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-  data:{
-    name:'code why',
-    students:[{
-      id:1,
-      name:'jack'
-    },
-    {
-      id:2,
-      name:'luxi'
-    },
-    {
-      id:3,
-      name:'daxi'
-    }
-  ],
-  counter:0
-  },
-  handleBtnclick(){
-    this.setData({
-      counter: this.data.counter+1
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        name:'code why',
+        students:[{
+          id:1,
+          name:'jack'
+        },
+        {
+          id:2,
+          name:'luxi'
+        },
+        {
+          id:3,
+          name:'daxi'
+        }
+      ],
+      counter:0
+      },
+      handleBtnclick(){
+        this.setData({
+          counter: this.data.counter+1
+        })
+      
+      },
+      handleBtnclick2(){
+        this.setData({
+          counter:this.data.counter-1
+        })
+      },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
+  },
+  onLoad: function () {
+    wx.request({
+      url: '',
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   
   },
-  handleBtnclick2(){
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      counter:this.data.counter-1
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
